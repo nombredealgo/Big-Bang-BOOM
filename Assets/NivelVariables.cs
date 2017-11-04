7f313c17bfee594ae29f632990596e643bdf2f6e
jamonserrano <sorrana.croac@gmail.com>
2017-11-04 22:39:56 +0100

    ddfsdhfskljfhs

---
+++ Assets/NivelVariables.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NivelVariables : MonoBehaviour {
+
+	[HideInInspector]
+	public int contadorDelantero = 0;
+	[HideInInspector]
+	public int contadorTrasero = 0;
+	[HideInInspector]
+	public bool terminado = false;
+
+	public string cursor;
+
+	// Use this for initialization
+	void Start () {
+		terminado = false;
+		cursor = "Nada";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
