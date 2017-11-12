ce419c356344cc70130eaf611bdc0911804d6ddd
Rebeca <al315155@uji.es>
2017-11-12 21:48:39 +0100

    tutorial texto
    faltarian los efectos visuales

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -1,20 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine;
+using System.Reflection;
 
 public class Tutorial : MonoBehaviour
 {
-	public Button buttonNext;
-	private int cont = 0;
+	public int cont;
+	public Text TextInPanel;
+
+	private List<String> textList = new List<String> {
+		"Bienvenid@ al tutorial de Big Bang BOOM!",
+		"Voy a explicarte cómo funciona este entorno de juego.",
+		"Este icono indica el número del nivel que estás jugando.",
+		"Este es tu avatar.",
+		"Y este es el contador que indica el tiempo que tienes para desarmar la bomba.",
+		"Tranqui@, en este nivel estará desactivado.",
+		"La bomba está compuesta por casillas.",
+		"Cada casilla puede o no contener un puzzle.",
+		"A medida que resuelvas los puzzles, nuevas casillas se desbloquearán.",
+		"A la derecha tienes la caja de herramientas.",
+		"Cada herramienta sirve para una función determinada.",
+		"Haciendo click sobre una herramienta la seleccionarás.",
+		"Continuemos."};
+	
+	//poner circulo
 
 	void Start(){
+		cont = 0;
+		TextInPanel.text = textList[cont];
 	}
 
-	void Update(){
+	public void Next(){
+		cont++;
+		TextInPanel.text = textList[cont];
 	}
-
-
 }
 
 
